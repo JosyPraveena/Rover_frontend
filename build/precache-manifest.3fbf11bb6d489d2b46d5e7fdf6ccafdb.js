11b85b45df0690159b637a2e89729ff6e6ff80f6
JosyPraveena <josypraveena@gmail.com>
2020-07-22 17:00:41 +0200

    env correcetion

---
+++ build/precache-manifest.3fbf11bb6d489d2b46d5e7fdf6ccafdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc89e34bf7cc901d5733fc987ded7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a565446056d3f7c7bc2",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "473056884f8295b974a9",
+    "url": "/static/css/main.3f1dedab.chunk.css"
+  },
+  {
+    "revision": "8a565446056d3f7c7bc2",
+    "url": "/static/js/2.ca1599d9.chunk.js"
+  },
+  {
+    "revision": "11553ead600ee17e0a1a09207195ea4b",
+    "url": "/static/js/2.ca1599d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473056884f8295b974a9",
+    "url": "/static/js/main.31d31e2a.chunk.js"
+  },
+  {
+    "revision": "7ef4217ac51a47b142d6",
+    "url": "/static/js/runtime-main.fb23388c.js"
+  }
+]);
